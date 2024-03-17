54de14dcce90cb3194ded1c24a568924ebe7ea15
Plus Pingya <plus.pingya@invenco.net>
2024-03-17 13:09:51 +0700

    implement equals() for Entity

--- src/ddd/core/Entity.ts
+++ src/ddd/core/Entity.ts
@@ -1,5 +1,9 @@
 import { createId } from '@paralleldrive/cuid2';
 
+const isEntity = (v: any): v is Entity<any> => {
+  return v instanceof Entity;
+}
+
 export default abstract class Entity<PropsType> {
   constructor(
     protected readonly props: PropsType,
@@ -8,5 +12,19 @@ export default abstract class Entity<PropsType> {
 
   get id(): string {
     return this._id;
+  }
+
+  equals(otherEntity?: Entity<PropsType>): boolean {
+    if (otherEntity === null || otherEntity === undefined) {
+      return false;
+    }
+    if (this === otherEntity) {
+      return true;
+    }
+    if (!isEntity(otherEntity)) {
+      return false;
+    }
+    
+    return this._id === otherEntity._id;
   }
 }
