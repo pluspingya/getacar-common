896a3231370ccefb2ee930498b11b872b70573fa
Plus Pingya <plus.pingya@invenco.net>
2025-01-27 12:11:19 +0700

    add set functionality to Entity

--- src/ddd/core/Entity.ts
+++ src/ddd/core/Entity.ts
@@ -1,8 +1,27 @@
 import { createId } from '@paralleldrive/cuid2';
+import Result from './Result';
 
 const isEntity = (v: any): v is Entity<any> => {
   return v instanceof Entity;
 }
+
+const FIRST_CHAR_REGEXP = /^./;
+
+const strToUpper = (s: string): string => (s || '').toUpperCase();
+
+const toSetterName = (propName): string => `set${propName.replace(FIRST_CHAR_REGEXP, strToUpper)}`
+
+type PropKey<SetterKey> = SetterKey extends `set${infer PropKey}` ? Uncapitalize<PropKey> : never;
+
+type SetterInput<SetterFunction> = SetterFunction extends (value: infer ValueType) => any ? ValueType: never;
+
+type SetterKeys<EntityTypeKeys> = Exclude<EntityTypeKeys extends `set${string}` ? EntityTypeKeys : never, 'set'>;
+
+type InternalSetterMap<EntityType extends { [key in EntitySetterKeys]: any }, EntitySetterKeys extends string> = {
+  [key in EntitySetterKeys as PropKey<key>]?: SetterInput<EntityType[key]>;
+}
+
+export type EntitySetterMap<EntityType> = InternalSetterMap<EntityType, SetterKeys<keyof EntityType>>;
 
 export default abstract class Entity<PropsType> {
   constructor(
@@ -25,5 +44,22 @@ export default abstract class Entity<PropsType> {
       return false;
     }
     return this._id === otherEntity._id;
+  }
+
+  set(
+    values: EntitySetterMap<this>, 
+    onlySetKeys?: (keyof EntitySetterMap<this>)[]
+  ): Result<void> {
+    const targetKeys = onlySetKeys || Object.keys(values);
+    const missingSetters = targetKeys.map(toSetterName).filter((setterName) => typeof this[setterName] !== 'function');
+    if (missingSetters.length) return Result.fail(`There is no setter: ${missingSetters.join(', ')}`);
+    for (const key of targetKeys) {
+      const value = values[key];
+      if (value === undefined) continue;
+      const setterName = toSetterName(key);
+      const result = this[setterName](value);
+      if (result instanceof Result && result.isFailure) return result;
+    }
+    return Result.ok();
   }
 }
