ab8d061bb526a7ad62760c7db6b80bb119fd43fe
Plus Pingya <plus.pingya@invenco.net>
2025-01-31 23:07:57 +0700

    Add IdName type

---
+++ src/domain/types/IdName.ts
@@ -0,0 +1,6 @@
+type IdName = {
+  id: string;
+  name: string;
+}
+
+export default IdName;
--- src/domain/types/index.ts
+++ src/domain/types/index.ts
@@ -1,1 +1,2 @@
+export { default as IdName } from './IdName';
 export { default as PaginationList } from './PaginationList';
