64dbf43a9cfba40bec2fe776853a67680c078c0b
Plus Pingya <plus.pingya@invenco.net>
2025-02-16 16:49:59 +0700

    Update ListingDTO to include shop and deliveryFees props

--- src/interface/DTOs/ListingDTO.ts
+++ src/interface/DTOs/ListingDTO.ts
@@ -1,14 +1,19 @@
 import ListingStatus from '../../domain/enums/ListingStatus';
+import ShopDTO from './ShopDTO';
 import CarDTO from './CarDTO';
 import LocationDTO from './LocationDTO';
+
+type DeliverFees = { [key: string]: number };
 
 type ListingDTO = {
   id: string;
   name: string;
   accountId: string;
+  shop: ShopDTO;
   car: CarDTO;
   description?: string;
   locations: LocationDTO[];
+  deliveryFees: DeliverFees;
   rentalPrice: number;
   hourlyPrice: number;
   vendorPrice: number;
