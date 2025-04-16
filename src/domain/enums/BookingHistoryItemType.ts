a25a04beacf3aacb9a23f62a7bebfc4b551eb54c
Plus Pingya <plus.pingya@invenco.net>
2025-04-16 22:37:12 +0700

    Tweak BookingHistoryItemType enum

--- src/domain/enums/BookingHistoryItemType.ts
+++ src/domain/enums/BookingHistoryItemType.ts
@@ -1,8 +1,11 @@
 enum BookingHistoryItemType {
+  REQUEST = 'REQUEST',
   CREATE = 'CREATE',
   UPDATE = 'UPDATE',
   APPROVE = 'APPROVE',
   REJECT = 'REJECT',
+  DELIVER = 'DELIVER',
+  COMPLETE = 'COMPLETE',
   CANCEL = 'CANCEL',
 };
 
