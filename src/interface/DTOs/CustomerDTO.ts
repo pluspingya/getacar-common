3d10f29de6f001ebb1f22e81a370e6688c07d4d2
Plus Pingya <plus.pingya@invenco.net>
2025-04-16 22:57:11 +0700

    Add CustomerDTO

---
+++ src/interface/DTOs/CustomerDTO.ts
@@ -0,0 +1,9 @@
+type CustomerDTO = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+  otherContact: string | null;
+};
+
+export default CustomerDTO;
--- src/interface/DTOs/index.ts
+++ src/interface/DTOs/index.ts
@@ -3,6 +3,7 @@ export { default as AddressDTO } from './AddressDTO';
 export { default as BookingDTO } from './BookingDTO';
 export { default as BookingHistoryItemDTO } from './BookingHistoryItemDTO';
 export { default as CarDTO } from './CarDTO';
+export { default as CustomerDTO } from './CustomerDTO';
 export { default as DeliveryFeeDTO } from './DeliveryFeeDTO';
 export { default as ImageDTO } from './ImageDTO';
 export { default as ListingDTO } from './ListingDTO';
