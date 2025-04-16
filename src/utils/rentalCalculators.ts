cfe58726a44ceffead5c836aea948870e05437f1
Plus Pingya <plus.pingya@invenco.net>
2025-04-16 21:28:33 +0700

    Add and implement findAdditionalFees util function

--- src/utils/rentalCalculators.ts
+++ src/utils/rentalCalculators.ts
@@ -1,3 +1,4 @@
+import { BookingAdditionalFee } from '../domain/enums';
 import { ListingDTO } from '../interface/DTOs';
 
 export type TotalTime = { 
@@ -24,4 +25,27 @@ export function findTotalRentalTime(pickUpDate: Date, returnDate: Date, listing:
 export function findTotalRentalPrice(totalTime: TotalTime, listing: ListingDTO): number {
   return totalTime.days * (listing.rentalPrice || listing.car.rentalPrice) + 
          totalTime.hours * (listing.hourlyPrice || listing.car.hourlyPrice);
+}
+
+export function findAdditionalFees({
+  pickUpLocationId,
+  returnLocationId,
+}: {
+  pickUpLocationId: string;
+  returnLocationId: string;
+  pickUpDate: Date;
+  returnDate: Date;
+}, listingDTO: ListingDTO): { [key in BookingAdditionalFee]?: number } {
+  const additionalFees: { [key in BookingAdditionalFee]?: number } = {};
+  const outOfFreeServiceAreaDeliveryFee = listingDTO.deliveryFees.find((deliveryFee) => deliveryFee.locationId === pickUpLocationId)
+  if (outOfFreeServiceAreaDeliveryFee) {
+    additionalFees[BookingAdditionalFee.OUT_OF_FREE_SERVICE_AREA_DELIVERY] = outOfFreeServiceAreaDeliveryFee.fee;
+  }
+  const outOfFreeServiceAreaReturnFee = listingDTO.deliveryFees.find((deliveryFee) => deliveryFee.locationId === returnLocationId);
+  if (outOfFreeServiceAreaReturnFee) {
+    additionalFees[BookingAdditionalFee.OUT_OF_FREE_SERVICE_AREA_RETURN] = outOfFreeServiceAreaReturnFee.fee;
+  }
+  //TODO: Consider charging for the out of operating hours
+  //In order to do this, shop must specify the operating hours and charges
+  return additionalFees;
 }
