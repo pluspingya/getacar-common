f2150feb2fd4b43b223f1be100abcba5ab3026e9
Plus Pingya <plus.pingya@invenco.net>
2025-06-29 11:31:18 +0700

    Add more data to UserTokenData

--- src/domain/types/UserTokenData.ts
+++ src/domain/types/UserTokenData.ts
@@ -5,11 +5,19 @@ export type UserTokenData = {
   email: string;
   name: string;
   activeAccountMembership: {
+    id: string;
     accountId: string;
     accountName: string;
     role: AccountMembershipRole;
   } | null;
+  accountMemberships: {
+    id: string;
+    accountId: string;
+    accountName: string;
+    role: AccountMembershipRole;
+  }[];
   shopMemberships: {
+    id: string;
     shopId: string;
     shopName: string;
     role: ShopMembershipRole;
