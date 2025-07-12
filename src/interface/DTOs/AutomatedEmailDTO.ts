cb97e0c3d97857094c51becd8eaaaee38a553fd1
Plus Pingya <plus.pingya@invenco.net>
2025-07-12 23:38:47 +0700

    Add languageCode prop to AutomatedEmailDTO

--- src/interface/DTOs/AutomatedEmailDTO.ts
+++ src/interface/DTOs/AutomatedEmailDTO.ts
@@ -1,9 +1,11 @@
+import { LanguageCode } from '../../domain';
 import AutomatedEmailType from '../../domain/enums/AutomatedEmailType';
 import EmailTemplateDTO from './EmailTemplateDTO';
 
 type AutomatedEmailDTO = {
   id: string;
   type: AutomatedEmailType;
+  languageCode: LanguageCode;
   enabled: boolean;
   emailTemplateId: string;
   emailTemplateName: string;
