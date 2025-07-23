80f4910788fc8df77b9738642b9d4c75e76d0b95
Plus Pingya <plus.pingya@invenco.net>
2025-07-23 21:58:52 +0700

    Index CarYearByModel

--- src/domain/consts/index.ts
+++ src/domain/consts/index.ts
@@ -9,4 +9,5 @@ export { default as CarTransmissions } from './CarTransmissions';
 export { default as CarTransmissionsByModel } from './CarTransmissionsByModel';
 export { default as CarTypes } from './CarTypes';
 export { default as CarTypesByModel } from './CarTypesByModel';
+export { default as CarYearByModel } from './CarYearByModel';
 export { default as LanguageCodes } from './LanguageCodes';
