1c2f25375b51b3209baca04fe29eac994f439ed6
Plus Pingya <plus.pingya@invenco.net>
2025-08-05 00:54:12 +0700

    Export EntitySetterMap

--- src/ddd/core/index.ts
+++ src/ddd/core/index.ts
@@ -1,4 +1,4 @@
-export { default as Entity } from './Entity';
+export { default as Entity, EntitySetterMap } from './Entity';
 export { default as Guard } from './Guard';
 export { default as Result } from './Result';
 export { default as UseCase } from './UseCase';
