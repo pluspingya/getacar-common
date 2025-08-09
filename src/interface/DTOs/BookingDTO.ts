376b262499cf3c631f43b85dbe699e1ce14f50d4
Plus Pingya <plus.pingya@invenco.net>
2025-08-09 23:51:28 +0700

    BookingDTO use CarDTO

--- src/interface/DTOs/BookingDTO.ts
+++ src/interface/DTOs/BookingDTO.ts
@@ -1,9 +1,11 @@
 import BaseBookingDTO from './BaseBookingDTO';
 import BookingHistoryItemDTO from './BookingHistoryItemDTO';
+import CarDTO from './CarDTO';
 import CustomerDTO from './CustomerDTO';
 import ShopDTO from './ShopDTO';
 
 type BookingDTO = BaseBookingDTO & {
+  car: CarDTO | undefined;
   shop: ShopDTO | undefined;
   customer: CustomerDTO;
   history: BookingHistoryItemDTO[];
