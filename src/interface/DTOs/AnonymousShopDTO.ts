635e2603e4a62733faeebe59a44d132a490a317b
Plus Pingya <plus.pingya@invenco.net>
2025-09-07 15:54:45 +0700

    Add more props to AnonymousShopDTO

--- src/interface/DTOs/AnonymousShopDTO.ts
+++ src/interface/DTOs/AnonymousShopDTO.ts
@@ -1,6 +1,13 @@
+import { Timezone } from '../../domain/types';
 import AfterHoursFeeDTO from './AfterHoursFeeDTO';
 
 type AnonymousShopDTO = {
+  operatingHoursStartTime: string;
+  operatingHoursEndTime: string;
+  operatingHoursTimezone: Timezone;
+  salesHoursStartTime: string;
+  salesHoursEndTime: string;
+  salesHoursTimezone: Timezone;
   afterHoursFees: AfterHoursFeeDTO[];
 }
 
